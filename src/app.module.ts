7035955b795b79026e027eb9c1cc3fbde31ba079
armandoMM <armando.matiasm99@gmail.com>
2023-03-27 12:14:38 -0600

    fix: import of usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
-import { UsuariosController } from './usuarios/usuarios.controller';
 
 @Module({
   imports: [UsuariosModule],
-  controllers: [AppController, UsuariosController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
